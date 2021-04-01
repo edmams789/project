5e9544ff06d91d3f87056e8acc48ae43060e49d2
Alexander <example@gmail.com>
2021-04-01 12:18:36 +0300

    Практика, ч4. item 24

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-"use strict";
+" use strict";
 
-let str = "some";
-let strObj = new String(str);
-console.log(typeof(str));
-console.log(typeof(strObj));
 
-console.dir([1, 2, 3]);
-
-const soldier = {
-    health: 400,
-    armor: 100,
-    sayHello: function() {
-        console.log("Hello");
-    }
-};
-const john = Object.create(soldier);
-
-// const john = {
-//     health: 100
-// };
-//устаревший формат (может встречаться)
-// john.__proto__ = soldier;
-
-// Object.setPrototypeOf(john, soldier); // == john.__proto__ = soldier;
-// console.log(john);
-// console.log(john.armor);
-john.sayHello();
