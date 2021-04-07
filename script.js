4620a1cc2d2f15a12623a48c2369169a734235f2
Alexander <example@gmail.com>
2021-04-07 11:49:22 +0300

    Задачи с собеседований, item 27

--- script.js
+++ script.js
@@ -1,54 +1,56 @@
 " use strict";
+// let x = 5; alert(x++)?
+let x = 5; alert(x++);
 
-// To String
+// [] + false - null + true?
+// - пустой массив превращается в пустую строку
+console.log([] + false - null + true); //NaN
+console.log(typeof([] + false)); //string ~ "false"
+console.log([] + false - null); //NaN
 
-// 1)
-console.log(typeof(String(null))); //string - outdated way
-console.log(String(null)); //null
-console.log(typeof(String(4))); //string
-console.log(String(4)); //4
+// let y = 1; let x = y = 2; alert(x);?
+let y = 1; let x1 = y = 2; alert(x1); //2
 
-// 2)
-console.log(typeof(5 + '')); //string
-console.log(typeof(null + '')); //string
+// [] + 1 + 2?
+console.log([] + 1 + 2);//"12"
 
-const num = 5;
-console.log("https://vk.com/catalog/" + num);
+// alert("1"[0])?
+alert("1"[0]); //"1" - обращение к строке по индексу
+alert("1"[2]); //undefined
 
-const fontSize = 26 + 'px';
+// 2 && 1 && null && 0 && undefined?
+// И (&&) запинается на лжи - в проверке доходим до null и останавливаемся
+console.log(2 && 1); //1
+console.log(1 && null); //null
+console.log(null && 0); //null
+console.log(0 && undefined); //0
+console.log(null && undefined); //null
+console.log(1 && 0); //0
+console.log(2 && 1 && null && 0 && undefined); //2, 1, null - остановились
 
-// To Number
+// !!(a && b) и (a && b)
+console.log(!!(1 && 2) === (1 && 2)); //false (!! - return boolean)
 
-// 1)
-console.log(typeof(Number('4'))); //number - outdated way
+// alert(null || 2 && 3 || 4)
+// ИЛИ (&&) запинается на правде
+alert(null || 2 && 3 || 4); //3 (смотреть таблицу приоритетов)
+// у && выше приоритет чем у ||
 
-// 2)
-console.log(typeof(+'5')); //number
+// a = [1, 2, 3]; b = [1, 2, 3]; a == b?
+const a = [1, 2, 3]; 
+const b = [1, 2, 3]; 
+console.log(a == b); //false
 
-// 3)
-console.log(typeof(parseInt("15px", 10))); //number
-console.log(typeof(parseInt("15px"))); //number
+// alert( +"infinity");?
+alert( +"infinity"); //NaN
+alert( +"Infinity"); //Infinity
 
+// "Ёжик" > "яблоко"?
+console.log("Ёжик" > "Яблоко"); //false
 
-let answ = +prompt("Hello", ""); //превращаем строковый тип данных в число
-
-// To boolean
-
-// 0, '', null, undefined, NaN;
-
-// 1)
-let switcher = null;
-if (switcher) {
-    console.log('Working...'); //
-}
-
-switcher = 1;
-if (switcher) {
-    console.log('Working...'); //Working...
-}
-
-// 2)
-console.log(typeof(Boolean('4'))); //boolean
-
-// 3)
-console.log(typeof(!!'4')); //boolean
+// 0 || "" || 2 || undefined || true || false?
+console.log(0 || "" || 2 || undefined || true || false); //2
+// 0 в логическом контексте - false
+// "" в логическом контексте - false
+// 2 в логическом контексте - true (запинается на правде)
+// дальше код не пойдёт
