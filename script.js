c88be2d7016650e48ea16fb7ed1286b7b2530e97
Alexander <example@gmail.com>
2021-04-23 11:59:17 +0300

    Скрипты и время их выполнения. setTimeout и setInterval, item 39

--- script.js
+++ script.js
@@ -1,48 +1,57 @@
 "use strict";
 
-const btns = document.querySelectorAll('button'),
-      wrapper = document.querySelector('.btn-block');
-// console.log(btns[0].classList.length);
-// console.log(btns[0].classList.item(1));
-// console.log(btns[1].classList.add('red'));
-// console.log(btns[0].classList.add('red', 'yellow'));
-// console.log(btns[0].classList.remove('blue'));
-// console.log(btns[0].classList.toggle('blue'));
+// const timerId = setTimeout(function() {
+//     console.log('Hello');
+// }, 2000);
 
-// if (btns[1].classList.contains('red')) {
-//     console.log('red');
-// }
+// const timerId = setTimeout(function(text) {
+//     console.log(text);
+// }, 2000, 'Hello');
 
-btns[0].addEventListener('click', () => {
-    // if (!btns[1].classList.contains('red')) {
-    //     btns[1].classList.add('red');
-    // } else {
-    //     btns[1].classList.remove('red');
-    // }
+const btn = document.querySelector('.btn');
+let timerId,
+    i = 0;
 
-    btns[1].classList.toggle('red');
-});
+function myAnimation() {
+    const elem = document.querySelector('.box');
+    let pos = 0;
+    const id = setInterval(frame, 10);
 
-// console.log(btns[0].className);
-
-wrapper.addEventListener('click', (event) => {
-    // if (event.target && event.target.tagName == "BUTTON") {
-    //     console.log('Hello');
-    // }
-    // if (event.target && event.target.classList.contains('blue')) {
-    //     console.log('Hello');
-    // }
-    if (event.target && event.target.matches("button.red")) {
-        console.log('Hello');
+    function frame() {
+        if(pos == 300) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";            
+        }
     }
-});
+}    
+btn.addEventListener('click', myAnimation);
 
-// btns.forEach(btn => {
-//     btn.addEventListener('click', () => {
-//     console.log('Hello'); 
-//     });
+
+
+
+// btn.addEventListener('click', () => {
+//     // const timerId = setTimeout(logger, 2000);
+//     timerId = setInterval(logger, 500);
 // });
 
-const btn = document.createElement('button');
-btn.classList.add('red');
-wrapper.append(btn);
+// clearInterval(timerId);
+
+// function logger() {
+//     console.log('text');
+// }
+
+// function logger() {
+//     if(i === 3) {
+//         clearInterval(timerId);
+//     }
+//     console.log('text');
+//     i++;
+// }
+
+// let id = setTimeout(function log() {
+//     console.log('Hello');
+//     id = setTimeout(log, 500);
+// });
